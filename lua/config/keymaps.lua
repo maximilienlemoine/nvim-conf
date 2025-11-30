12646cf0d189ce5e4e6e60ab1d806043e8ff48e5
Maximilien LEMOINE <maximilien.lemoine.pro@gmail.com>
2025-11-30 16:06:50 +0100

    restriction flèche directionnels

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function block_arrows()
+  vim.notify("🚫 Utilise hjkl !", vim.log.levels.WARN)
+end
+
+vim.keymap.set("", "<Up>", block_arrows)
+vim.keymap.set("", "<Down>", block_arrows)
+vim.keymap.set("", "<Left>", block_arrows)
+vim.keymap.set("", "<Right>", block_arrows)
